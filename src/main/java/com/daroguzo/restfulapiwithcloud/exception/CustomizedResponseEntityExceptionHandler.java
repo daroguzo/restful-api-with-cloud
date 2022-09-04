a9acda41bf34c7efe58c4a4bab391521149aba65
daroguzo <kjw2122@naver.com>
2022-09-04 19:14:40 +0900

    modify: optimize importation

--- src/main/java/com/daroguzo/restfulapiwithcloud/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/daroguzo/restfulapiwithcloud/exception/CustomizedResponseEntityExceptionHandler.java
@@ -12,7 +12,6 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 
 @ControllerAdvice
 @RestController
