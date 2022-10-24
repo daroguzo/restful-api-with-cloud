baa5b069b1f3e8f40bb7c7873354f0d9d0ef1313
daroguzo <kjw2122@naver.com>
2022-10-24 16:56:38 +0900

    FEAT: Swagger Documentation 구현

--- src/main/java/com/daroguzo/restfulapiwithcloud/user/User.java
+++ src/main/java/com/daroguzo/restfulapiwithcloud/user/User.java
@@ -1,6 +1,8 @@
 package com.daroguzo.restfulapiwithcloud.user;
 
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 import javax.persistence.Column;
@@ -12,6 +14,7 @@ import java.time.LocalDateTime;
 @Getter @Setter @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@ApiModel(description = "사용자 상세 정보를 위한 도메인 객체")
 @Entity
 public class User {
 
@@ -20,12 +23,15 @@ public class User {
     private Long id;
 
     @Column
+    @ApiModelProperty(notes ="사용자 이름을 입력해 주세요")
     private String name;
 
     @Column
+    @ApiModelProperty(notes ="사용자 비밀번호를 입력해 주세요")
     private String password;
 
     @Column
+    @ApiModelProperty(notes ="사용자 주민번호를 입력해 주세요")
     private String ssn;
 
     @Column
